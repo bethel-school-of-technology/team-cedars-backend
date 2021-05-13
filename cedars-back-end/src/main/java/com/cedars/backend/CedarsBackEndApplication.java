1b13e4b449789a85226ee4fadc763cb451207eaa
Michael Yaacoub <michael.yaacoub7@gmail.com>
2021-05-13 11:08:16 -0700

    initia back-end project

---
+++ cedars-back-end/src/main/java/com/cedars/backend/CedarsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.cedars.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CedarsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CedarsBackEndApplication.class, args);
+	}
+
+}
